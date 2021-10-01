915f2246783e60e545fc945edab5d4ef761dbff1
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-10-02 00:35:47 +0300

    adding AuthController Routes

--- routes/api.php
+++ routes/api.php
@@ -16,11 +16,20 @@ use App\Http\Controllers\API\AuthController;
 |
 */
 
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function () {
+	Route::post('register', [AuthController::class, 'register'])->name('api:register');
+    Route::post('login', [AuthController::class, 'login'])->name('api:login');
+    Route::any('logout', [AuthController::class, 'logout'])->name('api:logout');
+    Route::post('refresh', [AuthController::class, 'refresh'])->name('api:refresh');
+});
+
+
 Route::get('/highlighted', [UserController::class, 'highlighted'])->name('api:highlighted');
-Route::post('/login', [AuthController::class, 'login'])->name('api:login');
 
 Route::group(['middleware' => 'auth.jwt'], function () {
 	Route::get('/search/{keyword}', [UserController::class, 'search'])->name('api:search');
 	Route::get('/test', [UserController::class, 'test'])->name('api:test');
-	Route::post('logout', [AuthController::class, 'logout'])->name('api:logout');
 });
