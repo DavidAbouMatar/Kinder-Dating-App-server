fae657ed87151eb4ef5e0ccde3d646eea932d338
Wael Kaddoura <85300497+Wael-Kaddoura@users.noreply.github.com>
2021-10-02 00:35:32 +0300

    modifying AuthController

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -1,35 +1,156 @@
 <?php
+
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 use App\Models\User;
-use JWTAuth;
-use Auth;
 
+class AuthController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+		//the middleware will be run for all functions except login & register functions
+    }
 
-class AuthController extends Controller{
-	
-	function login(Request $request){
-		$data = $request->only("email", "password");
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
+        ]);
 
-		try{
-			if(!$token = JWTAuth::attempt($data)){
-				return json_encode(["error" => "Invalid Credentials"]);
-			}
-		}catch(JWTException $e){
-			return json_encode(["error" => "Error occured"]);
-		}
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        if (!$token = auth()->attempt($validator->validated())) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->createNewToken($token);
+    }
+
+    /**
+     * Register a User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required|string|between:2,100',
+            'last_name' => 'required|string|between:2,100',
+            'email' => 'required|string|email|max:100|unique:users',
+            'password' => 'required|string|confirmed|min:6',
+			'gender' => 'required|integer',
+			'interested_in' => 'required|integer',
+			'dob' => 'required',
+			'height' => 'required|integer',
+			'weight' => 'required|integer',
+			'nationality' => 'required|string|between:2,100',
+			'net_worth' => 'required|integer',
+			'currency' => 'required|string',
+			'bio' => 'required|string|between:2,200',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(array(
+                "status" => false,
+                "errors" => $validator->errors()
+            ), 400);
+        }
+
+		User::insert([
+			"user_type_id" => 2,
+			"first_name" => $request -> first_name,
+			"last_name" => $request -> last_name,
+			"email" => $request -> email,
+			"password" => bcrypt($request -> password),
+			"gender" => $request -> gender,
+			"interested_in" => $request -> interested_in,
+			"dob" => $request -> dob,
+			"height" => $request -> height,
+			"weight" => $request -> weight,
+			"nationality" => $request -> nationality,
+			"net_worth" => $request -> net_worth,
+			"currency" => $request -> currency,
+			"bio" => $request -> bio,
+			"is_highlighted" => 0,
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+
+	   ]);
+
 		
-		$user = Auth::user();
-		$user->token = $token;
-		return json_encode($user);
-		
-		
-	}
+        return response()->json([
+            'status' => true,
+            'message' => 'User successfully registered',
+        ], 201);
+    }
 
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'User successfully signed out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->createNewToken(auth()->refresh());
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function userProfile()
+    {
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function createNewToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => auth()->user()
+        ]);
+    }
 }
-
-?>
